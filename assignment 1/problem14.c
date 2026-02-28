f1fcbbe41601e8e27dc35a3b91e78074884f6b02
jeevanm-02-06 <jeevanm1225@gmail.com>
2026-02-28 13:30:15 +0530

    plm

---
+++ assignment 1/problem14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    printf("Enter you no:");
+    scanf("%d",&x);
+    y=((((x%100)%10)*100)+(((x%100)/10)*10)+(x/100));
+    printf("Result: %d",y);
+
+    return 0;
+}
